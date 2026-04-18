f6c304a0eb47875d9de263ca4ca9ab8abe498363
lessthen3 <lessthen3@starlightbrew.com>
2026-04-17 21:46:39 -0400

    moving back to windows machine
    yeah idk figuring this out still, not a fan of godot but w/e

--- scripts/EditorMain.cs
+++ scripts/EditorMain.cs
@@ -15,9 +15,18 @@ namespace Blueberry;
 
 public partial class EditorMain : Node
 {
+	TextureButton pm_ExitButton;
+	TextureButton pm_MinimizeButton;
+	TextureButton pm_ToggleWindowModeButton;
+
+
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public override void 
+		_Ready()
 	{
+		pm_MinimizeButton = GetNode<TextureButton>("");
+		pm_ToggleWindowModeButton = GetNode<TextureButton>("");
+		pm_ExitButton = GetNode<TextureButton>("");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
